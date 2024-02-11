2636d27da5f7fdfdb70e4a82306f09326dcb9f9b
Plaginnnn <mulyukov.rasim@mail.ru>
2024-02-11 05:15:07 +0300

    deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./pages/**/*.{html,js}', './components/**/*.{html,js}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
-
