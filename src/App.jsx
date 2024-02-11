d3c9fc6a5a106a4e6581fadb259460dce999df89
Plaginnnn <mulyukov.rasim@mail.ru>
2024-02-11 06:03:00 +0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
 import Main from './pages/Main/Main'
 
 export const App = () => {
 	return (
 		<div>
-			<BrowserRouter>
-				<Routes>
-					<Route path='/' element={<Main />} />
-				</Routes>
-			</BrowserRouter>
+			<Main />
 		</div>
 	)
 }
